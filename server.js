46bfdb17c55e668552ff1ad02262b41aa16517fd
Laptop Nimatuj <qetamajapp@gmail.com>
2019-06-26 22:09:25 -0600

    proyecto usando handlebars

--- server.js
+++ server.js
@@ -1,28 +1,63 @@
 const express = require('express')
-const app = express()
+const app = express();
  //Middleware un callback que se va a ejecutar siempre que hAYA UNA peticion
  //
  //
- 
- app.use(express.static(__dirname + '/public'));
+ var hbs = require('hbs');
+require('./hbs/helpers')
 
-/*app.get('/',  (req, res)=> {
-  	let salida = {
+const port = process.env.PORT || 3000;
+
+ app.use(express.static(__dirname + '/public'));
+ //Carpeta publica accesible a culquier prsona
+ //__dirname => path al proyecto
+hbs.registerPartials(__dirname + '/views/parciales' );
+//Parciales
+//Usar handlebar como engine
+app.set('view engine', 'hbs');
+
+
+app.get('/',  (req, res)=> {
+
+	res.render('home', {
+		nombre: "corina",
+	}); //Render delhome
+  	/*let salida = {
 		nombre: "Corina",
 		edad: 23,
 		url: req.url
 	}
 	res.send(salida);
-  //res.send('Hola Mundo')
+  //res.send('Hola Mundo')*/
 });
 
+app.get('/about',  (req, res)=> {
 
+	res.render('about'); //Render delhome
+  	/*let salida = {
+		nombre: "Corina",
+		edad: 23,
+		url: req.url
+	}
+	res.send(salida);
+  //res.send('Hola Mundo')*/
+});
+
+/*
 app.get('/data',  (req, res)=> {
 
   res.send('Hola Mundo data')
 });
 */
  
-app.listen(3000, ()=>{
-	console.log("Escuchando peticiones en el puerto 3000");
+app.listen(port, ()=>{
+	console.log("Escuchando peticiones en el puerto "+ port);
 })
+
+
+
+/*
+nodemon escucha todas las extensiones
+$ nodemon server.js -e js, hbs,html,css
+
+*/
